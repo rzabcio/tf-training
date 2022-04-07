115d9d1f82f648766ab1f38759e1c1813c8adfa5
Jakub Głazik <rzabcio@gmail.com>
2022-04-07 22:26:55 +0200

    Creating service account.

---
+++ 00-create-service-account.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+USER_NAME=terraform
+KEY_FILE=sa-$USER_NAME-key.json
+
+project=$(gcloud config get-value project 2> /dev/null)
+sa_email=$(gcloud iam service-accounts list --format="value(email)" | grep $USER_NAME)
+if [ -z $terraform_user ]; then
+	echo "+++ user needs to be created"
+	gcloud iam service-accounts create terraform
+	sa_email=$(gcloud iam service-accounts list --format="value(email)" | grep $USER_NAME)
+else
+	echo "    user exists"
+fi
+gcloud projects add-iam-policy-binding $project --member=serviceAccount:$sa_email --role=roles/editor
+
+if [ ! -f $KEY_FILE ]; then
+	gcloud iam service-accounts keys create $KEY_FILE --iam-account=$sa_email
+fi
+
+
